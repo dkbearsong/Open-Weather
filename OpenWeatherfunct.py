d440fb3609013a12c4d1d9ce74b2c8179f465e06
Dereck Bearsong <firestorm117@gmail.com>
2020-01-05 22:43:26 +0000

    Tested and verified Weather page will produce web results

--- OpenWeatherfunct.py
+++ OpenWeatherfunct.py
@@ -4,12 +4,9 @@ import requests
 
 
 class openweather:
-#  check = owsql.checkSQL()
-#  load = owsql.loadSQL()
   wApiKey = 'b9675c37d1e97547e29ffa8b854dca61'
   gApiKey = '8191c92c550f494bba3b69fc55ed8310'
-#  cur = load.createSQL()
-
+  
   def convert_time(self, time):
     # converts UTC time to GMT time
     return(datetime.utcfromtimestamp(time).strftime('%H:%M:%S'))
@@ -17,28 +14,6 @@ class openweather:
   def convert_date(self, time):
     # Converts date from UTC time
     return(datetime.utcfromtimestamp(time).strftime('%m-%d-%Y'))
-
-#  def get_location(self, loc):
-    #need a way to know what I am looking for with the provided location information
-#    if len(loc) > 1: 
-#      search = '&latitude=' + str(loc['latitude']) + '&longitude=' + str(loc['longitude'])
-#    else:
-#      search = '&' + loc + '=' + str(loc.get())
-#    geo_link = 'https://api.ipgeolocation.io/timezone?apiKey=' + self.gApiKey + search
-#    res = requests.get(geo_link)
-#    geo_api = res.json()
-#    coordinates = {
-#     'longitude': geo_api['geo']['longitude'],
-#     'latitude': geo_api['geo']['latitude'],
-#     'city': geo_api['geo']['city'],
-#     'state_provence': geo_api['geo']['state_prov'],
-#     'county' : geo_api['geo']['district'],
-#     'country': geo_api['geo']['country_name'],
-#     'timezone' : geo_api['timezone'],
-#     'timezone_offset': geo_api['timezone_offset']
-#    }
-#    coordinates['id'] = self.check.checkLocation(coordinates)
-#    return(coordinates)
 
   def process_today(self, data, coord):
     #takes JSON form API and seperates it into different dictionaries, converts time, and loads into another dictionary to return
@@ -78,37 +53,16 @@ class openweather:
   def get_current(self, coor):
     # Connects to openweather api and retrieves json data for today's forecast
     # returns prodata
-#    result = self.check.checkToday(self.cur, coor)
-#    if result != null:
-#      return(result)
-#    else:
-#      weblink = 'http://api.openweathermap.org/data/2.5/weather?q=' + coor['city'] + '&units=imperial&appid=' + self.wApiKey
-#      res = requests.get(weblink)
-#      data = res.json()
-#      procData = self.process_today(data, coor)
     weblink = 'http://api.openweathermap.org/data/2.5/weather?q=' + coor['city'] + '&units=imperial&appid=' + self.wApiKey
     res = requests.get(weblink)
     data = res.json()
     procData = self.process_today(data, coor)
-#      self.load.loadToday(self.cur, procData) 
-#      return(procData)
     return(procData)
 
   def get_fiveDay(self, coor):
     # Connects to openweather api and retrieves json data for five day forecast
-    # #12 get_fiveDay checks to see if five day forecast is already in the SQL database and returns it if it is. If not, it queries the webapi to retreive the information and then loads it to the SQL database, then returns it
-#    result = self.check.checkForecast(self.cur, coor)
-#    if result != null:
-#      return(result)
-#    else:
-#      weblink = 'http://api.openweathermap.org/data/2.5/forecast?q=' + coor['city'] + '&units=imperial&appid=' + self.wApiKey
-#      res = requests.get(weblink)
-#      data = res.json()
-#      procData = self.process_fiveDay(data, coor)
     weblink = 'http://api.openweathermap.org/data/2.5/forecast?q=' + coor['city'] + '&units=imperial&appid=' + self.wApiKey
     res = requests.get(weblink)
     data = res.json()
     procData = self.process_fiveDay(data, coor)
-#      self.load.loadForecast(self.cur, procData)
-#      return(procData)
     return(procData)
--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-import requests, OpenWeatherfunct as owf
-from flask import Flask, render_template, jsonify, redirect, url_for,request, redirect
-from flask import make_response
+import OpenWeatherfunct as owf
+from flask import Flask, render_template, request
 from forms import FindWeatherForm
-
-weather = owf.openweather()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0f403f865aef5523b4cbbe4ba4ba8203'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-  form = FindweatherForm()
+  form = FindWeatherForm()
+  weather = owf.openweather()
   if form.validate_on_submit():
-    coord = [request.form.get('city'), request.form.get('state'), request.form.get('country')]      
-    flash(f'Weather data retrieved for {form.city.data}', 'success')
+    coord = {'city': request.form.get('city'), 'state': request.form.get('state'), 'country':request.form.get('country')}
     today=weather.get_current(coord)
     fiveDay=weather.get_fiveDay(coord)
     allWeather = [today, fiveDay]
-  return render_template('owweb.html', title='Weather', form=form, weather=allWeather)
+    return render_template('weather.html', title='Weather', form=form, allWeather=allWeather)
+  return render_template('owweb.html', title='Weather', form=form)
+
+@app.route("/weather", methods=['GET', 'POST'])
+def weather():
+  form = FindWeatherForm()
+  weather = owf.openweather()
+  if form.validate_on_submit():
+    coord = {'city': request.form.get('city'), 'state': request.form.get('state'), 'country':request.form.get('country')}
+    today=weather.get_current(coord)
+    fiveDay=weather.get_fiveDay(coord)
+    allWeather = [today, fiveDay]
+    return render_template('weather.html', title='Weather', form=form, allWeather=allWeather)
+  return render_template('weather.html', title='Weather', form=form, allWeather=allWeather)
 
 if __name__ == "__main__":
     app.run(debug = True)
